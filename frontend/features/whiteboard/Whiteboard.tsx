5d2509e84d7dbc7245e02af340315f7eead1db3c
jadenbn <jlambn@shaw.ca>
2026-08-22 12:51:34 -0700

    mess around w tldraw api

--- frontend/features/whiteboard/Whiteboard.tsx
+++ frontend/features/whiteboard/Whiteboard.tsx
@@ -1,9 +1,45 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import { useRef } from "react";
+import { createShapeId, Editor, toRichText, useEditor } from "tldraw";
 
-const Tldraw = dynamic(() => import("tldraw").then((module) => module.Tldraw), { ssr: false });
+const Tldraw = dynamic(() => import("tldraw").then((module) => module.Tldraw), {
+  ssr: false,
+});
 
 export function Whiteboard() {
-  return <Tldraw />;
+  const editor = useRef<Editor>(null);
+
+  const drawThing = () => {
+    const id = createShapeId("firs tshape");
+    editor.current?.createShape({
+      id,
+      type: "geo",
+      x: 128,
+      y: 128,
+      props: {
+        geo: "rectangle",
+        w: 120,
+        h: 120,
+        dash: "draw",
+        color: "black",
+        size: "m",
+        richText: toRichText("hello!!"),
+      },
+    });
+  };
+
+  return (
+    <>
+      <button className="mt-10" onClick={drawThing}>
+        asdf
+      </button>
+      <Tldraw
+        onMount={(edit) => {
+          editor.current = edit;
+        }}
+      />
+    </>
+  );
 }
